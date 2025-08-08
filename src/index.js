cc43b863daac1f4c01e8eedf714907d1e0fc145b
Tegakist <today.is.another.day.157.6cm@gmail.com>
2025-08-08 14:01:25 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import FlowBlock from "./FlowBlock";
+import FlowBlock from "./FlowBlock.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
