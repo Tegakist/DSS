7725b229398f2827302697eac30e42eb7630ea71
Tegakist <today.is.another.day.157.6cm@gmail.com>
2025-08-08 11:40:47 +0900

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import FlowBlock from "./FlowBlock";
 
-body {
-  @apply bg-gray-100;
-}
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <FlowBlock />
+  </React.StrictMode>
+);
